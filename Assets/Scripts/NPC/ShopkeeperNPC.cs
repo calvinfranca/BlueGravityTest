f1d2d2391976d04f7ae9403373a46c1146c50b5c
calvinfranca <calvin.franca@gmail.com>
2023-09-03 14:23:03 -0300

    Name change
    VendorNPC is now ShopkeeperNPC

--- Assets/Scripts/NPC/ShopkeeperNPC.cs
+++ Assets/Scripts/NPC/ShopkeeperNPC.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VendorNPC : MonoBehaviour
+public class ShopkeeperNPC : MonoBehaviour
 {
     #region Variables
 
